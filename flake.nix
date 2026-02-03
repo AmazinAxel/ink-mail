64eb82a5bd4e2670baf38918966b34b95aa9b6c4
AmazinAxel <axel@amazinaxel.com>
2026-02-03 10:46:24 -0800

    fix: fix devshell and make program actually visible

--- flake.nix
+++ flake.nix
@@ -1,30 +1,31 @@
 {
   description = "Ink Mail Kindle devshell";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-
-      target = "kindlehf";
-
-      cross = pkgs.pkgsCross.${{
-        kindlehf = "armv7l-hf-multiplatform";
-        kindlepw2 = "armv7l-multiplatform";
-      }.${target}};
-    in
-    {
+    in {
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = with pkgs; [
+          # For build
           meson
           ninja
           pkg-config
           gtk2
           tinyxxd
-          #cross.buildPackages.gcc
+
+          # Fix warnings
+          gtk-engine-murrine
+          gnome-themes-extra
         ];
+        
+        # Export themes to fix adwaita warning
+        shellHook = ''
+          export GTK_PATH="${pkgs.gtk-engine-murrine}/lib/gtk-2.0:${pkgs.gnome-themes-extra}/lib/gtk-2.0"
+        '';
       };
     };
 }
--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,7 @@ int main(int argc, char *argv[]) {
   GtkWidget *vbox = gtk_vbox_new(FALSE, 0);
   gtk_container_add(GTK_CONTAINER(window), vbox);
 
+  gtk_widget_show_all(window);
   gtk_main();
   return 0;
 }

4ff29f71468f43c2b5137b8c5a06c10a4b85c9aa
AmazinAxel <axel@amazinaxel.com>
2026-02-03 14:37:36 -0800

    feat: intellisense

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ This app is designed to have hardcoded email server login details. This means yo
 
 ## Develop using Nix
 
-Run `nix develop` to run the ad hoc devshell. Run `meson setup build` and then use `meson compile -C build` to build.
+In the project directory, run `nix develop` to run the ad hoc devshell. Run `meson setup build` and then use `meson compile -C build` to build. Run your IDE instance in this devshell to have working intellisense.
 
 ## Adding icons
 
--- src/main.cpp
+++ src/main.cpp
@@ -11,24 +11,17 @@ struct emailData {
 
 int main(int argc, char *argv[]) {
   gtk_init(&argc, &argv);
-
   GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  gtk_window_set_title(GTK_WINDOW(window), "Ink Mail");
-  gtk_window_set_default_size(GTK_WINDOW(window), 600, 800);
-  gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+  GtkWindow *windowObj = GTK_WINDOW(window);
+  gtk_window_set_title(windowObj, "Ink Mail");
+  gtk_window_set_default_size(windowObj, 600, 800);
+  gtk_window_set_position(windowObj, GTK_WIN_POS_CENTER);
   gtk_container_set_border_width(GTK_CONTAINER(window), 5);
-  gtk_window_set_icon_from_file(GTK_WINDOW(window), "./icons/mail.png", NULL);
 
-  // todo fix icons
   GtkWidget *vbox = gtk_vbox_new(FALSE, 0);
   gtk_container_add(GTK_CONTAINER(window), vbox);
-
-  gtk_window_set_icon_from_file(GTK_WINDOW(window), "./icons/mail.png", NULL);
-
-  GtkWidget *icon = gtk_image_new_from_file("./icons/mail.png");
-  gtk_box_pack_start(GTK_BOX(vbox), icon, FALSE, FALSE, 0);
 
   gtk_widget_show_all(window);
   gtk_main();
   return 0;
-}
+};
